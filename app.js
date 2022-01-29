692cf0cbb35c2381cf241452ff4499e103520895
ansarulSohan <54618940+ansarulSohan@users.noreply.github.com>
2022-01-29 23:09:32 +0600

    installed and tested sequelize connection

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+require("express-async-errors");
 const express = require("express");
+const exphbs = require("express-handlebars");
+const sequelize = require("./config/database");
+
 const app = express();
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Success!!!"))
+  .catch("Failed");
+
+app.get("/", (req, res) => res.send("INDEX"));
 
 const PORT = process.env.PORT || 3000;
 
---
+++ config/database.js
@@ -0,0 +1,5 @@
+const Sequelize = require("sequelize");
+
+module.exports = new Sequelize(
+  "postgres://postgres:ndub@4321@localhost:5432/codegig"
+);
