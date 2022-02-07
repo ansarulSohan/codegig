e177968ace7799a0f55e2869fac15bdede8f2dad
ansarulSohan <54618940+ansarulSohan@users.noreply.github.com>
2022-02-08 00:00:04 +0600

    Added chalk

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
-const Sequelize = require("sequelize");
-
+const Sequelize = require('sequelize');
 module.exports = new Sequelize(
-  "postgres://postgres:ndub@4321@localhost:5432/codegig"
+  'postgres://postgres:ndub@4321@localhost:5432/codegig'
 );
